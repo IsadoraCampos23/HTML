b0b11993778f670d8b60be4dd7b69f39ad2b3473
IsadoraCampos23 <katembo2317@gmail.com>
2022-12-06 14:41:27 +0100

    comite

---
+++ Proj Marketing/script.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+var iconMenu = document.querySelector('nav .memu-icon img');
+
+menuBar.addEventListener('click', function(){
+
+
+
+
+   menu.classList.toggle('active');
+
+});
